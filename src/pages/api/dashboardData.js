db8e5e0aec1ed7732c20ea19f53855044907c5a6
oDANKFACE <darien.r.weller@gmail.com>
2023-12-11 18:11:45 -0700

    Fixed What I Broke

--- src/pages/api/dashboardData.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from '../../db/mongoose';
-
-export default async function handler(req, res) {
-    await dbConnect();
-
-    // Fetch data from your database and return it
-    res.status(200).json({ data: 'Your dashboard data' });
-}
