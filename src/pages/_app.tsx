941b5314d3cb0f54c27417047a181aaf5df90c77
oDANKFACE <darien.r.weller@gmail.com>
2023-12-11 20:48:44 -0700

    AppProps
    in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../styles/global.css';
+import { AppProps } from 'next/app';
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
 
